62dfee364ff6411eca999369328ccf5b96bf4f7e
RenatFaiz <renatf87@gmail.com>
2019-12-14 12:16:00 +0400

    2nd webapp prototype created!

--- src/main/java/ru/itpark/servlet/Servlet.java
+++ src/main/java/ru/itpark/servlet/Servlet.java
@@ -6,19 +6,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet("/text-searching")
+@WebServlet("/search")
 public class Servlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setContentType("text/html");
 
+        //String searchText = req.getParameter("text");
+
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        super.doPost(req, resp);
+        resp.setContentType("text/html");
+        String text = req.getParameter("text");
+        String file = req.getParameter("file");
+
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.println("<p>Searching text: " + text + "</p>");
+            writer.println("<p>File for search: " + file + "</p>");
+        }
     }
 }
